c92ac88540df0064693af02bf33a9ad8cb04b83d
Michael Lavelle <michael@lavelle.name>
2014-02-12 22:00:53 +0700

    #19 Correcting typos in unit test, eg. UCT instead of UTC

--- src/test/java/org/socialsignin/spring/data/dynamodb/repository/query/AbstractDynamoDBQueryCriteriaUnitTests.java
+++ src/test/java/org/socialsignin/spring/data/dynamodb/repository/query/AbstractDynamoDBQueryCriteriaUnitTests.java
@@ -25,17 +25,17 @@ public abstract class AbstractDynamoDBQueryCriteriaUnitTests<C extends AbstractD
 		DateFormat estDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
 		estDateFormat.setTimeZone(TimeZone.getTimeZone("EST"));
 		DateFormat utcDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
-		utcDateFormat.setTimeZone(TimeZone.getTimeZone("UCT"));
+		utcDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
 		
 		// Generate a Date object using a fixed EST date string
 		String dateInESTString = "2014-02-12T02:34:00.000Z";
 		Date now = estDateFormat.parse(dateInESTString);
 
 		// Generate a date string for this Date in UTC
-		String dateInUCTString = utcDateFormat.format(now);
+		String dateInUTCString = utcDateFormat.format(now);
 
 		// Sanity check - confirm that the EST and UTC strings aren't equal
-		Assert.assertNotEquals(estDateFormat, dateInUCTString);
+		Assert.assertNotEquals(dateInESTString, dateInUTCString);
 	
 		List<AttributeValue> attributeValueList = new ArrayList<AttributeValue>();
 
@@ -45,7 +45,7 @@ public abstract class AbstractDynamoDBQueryCriteriaUnitTests<C extends AbstractD
 		AttributeValue resultingValue = attributeValueList.get(0);
 		
 		// Ensuring that the resulting AttributeValue is encoded as a UTC string
-		Assert.assertEquals(dateInUCTString,resultingValue.getS());
+		Assert.assertEquals(dateInUTCString,resultingValue.getS());
 	}
 	
 	@Test
@@ -55,17 +55,17 @@ public abstract class AbstractDynamoDBQueryCriteriaUnitTests<C extends AbstractD
 		DateFormat estDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
 		estDateFormat.setTimeZone(TimeZone.getTimeZone("EST"));
 		DateFormat utcDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
-		utcDateFormat.setTimeZone(TimeZone.getTimeZone("UCT"));
+		utcDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
 		
 		// Generate a Date object using a fixed EST date string
 		String dateInESTString = "2014-02-12T02:34:00.000Z";
 		Date now = estDateFormat.parse(dateInESTString);
 
 		// Generate a date string for this Date in UTC
-		String dateInUCTString = utcDateFormat.format(now);
+		String dateInUTCString = utcDateFormat.format(now);
 
 		// Sanity check - confirm that the EST and UTC strings aren't equal
-		Assert.assertNotEquals(estDateFormat, dateInUCTString);
+		Assert.assertNotEquals(dateInESTString, dateInUTCString);
 	
 		List<AttributeValue> attributeValueList = new ArrayList<AttributeValue>();
 
@@ -73,7 +73,7 @@ public abstract class AbstractDynamoDBQueryCriteriaUnitTests<C extends AbstractD
 		dateList.add(now);
 		
 		List<String> dateStringList = new ArrayList<String>();
-		dateStringList.add(dateInUCTString);
+		dateStringList.add(dateInUTCString);
 		
 		// Add "now" as an attribute value - a Date object originally generated from EST string
 		criteria.addAttributeValue(attributeValueList , dateList, "someDateProperty", Date.class, true);
