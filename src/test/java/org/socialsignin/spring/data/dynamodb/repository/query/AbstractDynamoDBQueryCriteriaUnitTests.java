185937568a65485d319c5000e7eddecda53f1eee
Michael Lavelle <michael@lavelle.name>
2014-02-12 15:33:21 +0700

    Adding (currently failing) test for addAttributeValue in AbstractDynamoDBQueryCriteria to cover scenario where attribute value is a list of dates

--- src/test/java/org/socialsignin/spring/data/dynamodb/repository/query/AbstractDynamoDBQueryCriteriaUnitTests.java
+++ src/test/java/org/socialsignin/spring/data/dynamodb/repository/query/AbstractDynamoDBQueryCriteriaUnitTests.java
@@ -48,4 +48,40 @@ public abstract class AbstractDynamoDBQueryCriteriaUnitTests<C extends AbstractD
 		Assert.assertEquals(dateInUCTString,resultingValue.getS());
 	}
 	
+	@Test
+	public void addAttributeValueTest_WhenValueIsDateCollection() throws ParseException
+	{
+		// Setup date formats for EST and UCT
+		DateFormat estDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+		estDateFormat.setTimeZone(TimeZone.getTimeZone("EST"));
+		DateFormat utcDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+		utcDateFormat.setTimeZone(TimeZone.getTimeZone("UCT"));
+		
+		// Generate a Date object using a fixed EST date string
+		String dateInESTString = "2014-02-12T02:34:00.000Z";
+		Date now = estDateFormat.parse(dateInESTString);
+
+		// Generate a date string for this Date in UTC
+		String dateInUCTString = utcDateFormat.format(now);
+
+		// Sanity check - confirm that the EST and UTC strings aren't equal
+		Assert.assertNotEquals(estDateFormat, dateInUCTString);
+	
+		List<AttributeValue> attributeValueList = new ArrayList<AttributeValue>();
+
+		List<Date> dateList = new ArrayList<Date>();
+		dateList.add(now);
+		
+		List<String> dateStringList = new ArrayList<String>();
+		dateStringList.add(dateInUCTString);
+		
+		// Add "now" as an attribute value - a Date object originally generated from EST string
+		criteria.addAttributeValue(attributeValueList , dateList, "someDateProperty", Date.class, true);
+		
+		AttributeValue resultingValue = attributeValueList.get(0);
+		
+		// Ensuring that the resulting AttributeValue is encoded as a UTC string
+		Assert.assertEquals(dateStringList,resultingValue.getSS());
+	}
+	
 }
