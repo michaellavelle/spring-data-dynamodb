d324358b64b7dd14cadd0d4ae64c73211c2ba631
Michael Lavelle <michael@lavelle.name>
2014-07-07 23:25:01 +0100

    #34 Adding unit tests to ensure batchDelete and batchSave methods call appropriate DynamoDBMapper methods

--- src/test/java/org/socialsignin/spring/data/dynamodb/core/DynamoDBTemplateUnitTests.java
+++ src/test/java/org/socialsignin/spring/data/dynamodb/core/DynamoDBTemplateUnitTests.java
@@ -1,10 +1,15 @@
 package org.socialsignin.spring.data.dynamodb.core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.socialsignin.spring.data.dynamodb.domain.sample.Playlist;
 import org.socialsignin.spring.data.dynamodb.domain.sample.User;
@@ -29,6 +34,22 @@ public class DynamoDBTemplateUnitTests {
 		this.dynamoDBTemplate = new DynamoDBTemplate(dynamoDB);
 		this.dynamoDBTemplate.dynamoDBMapper = dynamoDBMapper;
 		
+	}
+	
+	@Test
+	public void testBatchDelete_CallsCorrectDynamoDBMapperMethod()
+	{
+			Iterable<User> users = new ArrayList<User>();
+			dynamoDBTemplate.batchDelete(users);
+			Mockito.verify(dynamoDBMapper).batchDelete(Mockito.any(List.class));
+	}
+	
+	@Test
+	public void testBatchSave_CallsCorrectDynamoDBMapperMethod()
+	{
+			Iterable<User> users = new ArrayList<User>();
+			dynamoDBTemplate.batchSave(users);
+			Mockito.verify(dynamoDBMapper).batchSave(Mockito.any(List.class));
 	}
 	
 	@Test
