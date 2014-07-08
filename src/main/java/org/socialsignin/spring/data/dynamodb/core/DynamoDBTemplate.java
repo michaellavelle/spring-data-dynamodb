51dddd67a5709289fcc965fbb71a556167683ed9
Michael Lavelle <michael@lavelle.name>
2014-07-08 14:07:30 +0100

    #37 Fixing the root cause of NPEs when config is null on DynamoDBTemplate by fixing bug where default config was not being assigned correctly

--- src/main/java/org/socialsignin/spring/data/dynamodb/core/DynamoDBTemplate.java
+++ src/main/java/org/socialsignin/spring/data/dynamodb/core/DynamoDBTemplate.java
@@ -61,7 +61,7 @@ public class DynamoDBTemplate implements DynamoDBOperations,ApplicationContextAw
 				amazonDynamoDB, dynamoDBMapperConfig);
 		if (dynamoDBMapperConfig == null)
 		{
-			dynamoDBMapperConfig = DynamoDBMapperConfig.DEFAULT;
+			this.dynamoDBMapperConfig = DynamoDBMapperConfig.DEFAULT;
 		}
 	}
 	
@@ -195,7 +195,7 @@ public class DynamoDBTemplate implements DynamoDBOperations,ApplicationContextAw
 
 	@Override
 	public String getOverriddenTableName(String tableName) {
-		if (dynamoDBMapperConfig != null && dynamoDBMapperConfig.getTableNameOverride() != null) {
+		if (dynamoDBMapperConfig.getTableNameOverride() != null) {
 			if (dynamoDBMapperConfig.getTableNameOverride().getTableName() != null) {
 				tableName = dynamoDBMapperConfig.getTableNameOverride().getTableName();
 			} else {
