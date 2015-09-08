9f52041f9397bd24f991bd549009e0f2a707c7e8
Guillaume Larose <guills@guillsowns.me>
2015-09-08 18:04:17 -0400

    upgrade to boot 1.0.3M5 and fix RepoFactory for latest data-jpa-commons

--- src/main/java/org/socialsignin/spring/data/dynamodb/repository/support/DynamoDBRepositoryFactory.java
+++ src/main/java/org/socialsignin/spring/data/dynamodb/repository/support/DynamoDBRepositoryFactory.java
@@ -1,17 +1,14 @@
 /*
  * Copyright 2013 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package org.socialsignin.spring.data.dynamodb.repository.support;
 
@@ -23,28 +20,28 @@ import org.socialsignin.spring.data.dynamodb.core.DynamoDBOperations;
 import org.socialsignin.spring.data.dynamodb.repository.DynamoDBCrudRepository;
 import org.socialsignin.spring.data.dynamodb.repository.query.DynamoDBQueryLookupStrategy;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
+import org.springframework.data.repository.core.RepositoryInformation;
 import org.springframework.data.repository.core.RepositoryMetadata;
 import org.springframework.data.repository.core.support.RepositoryFactorySupport;
 import org.springframework.data.repository.query.QueryLookupStrategy;
 import org.springframework.data.repository.query.QueryLookupStrategy.Key;
-
 
 /**
  * @author Michael Lavelle
  */
 public class DynamoDBRepositoryFactory extends RepositoryFactorySupport {
 
-	private DynamoDBOperations dynamoDBOperations;
+	private final DynamoDBOperations dynamoDBOperations;
 
 	public DynamoDBRepositoryFactory(DynamoDBOperations dynamoDBOperations) {
 		this.dynamoDBOperations = dynamoDBOperations;
-		
+
 	}
 
 	@Override
 	public <T, ID extends Serializable> DynamoDBEntityInformation<T, ID> getEntityInformation(final Class<T> domainClass) {
 
-		DynamoDBEntityMetadataSupport<T, ID> metadata = new DynamoDBEntityMetadataSupport<T, ID>(domainClass);
+		final DynamoDBEntityMetadataSupport<T, ID> metadata = new DynamoDBEntityMetadataSupport<T, ID>(domainClass);
 		return metadata.getEntityInformation();
 	}
 
@@ -54,8 +51,7 @@ public class DynamoDBRepositoryFactory extends RepositoryFactorySupport {
 	}
 
 	/**
-	 * Callback to create a {@link JpaRepository} instance with the given
-	 * {@link EntityManager}
+	 * Callback to create a {@link JpaRepository} instance with the given {@link EntityManager}
 	 * 
 	 * @param <T>
 	 * @param <ID>
@@ -64,20 +60,14 @@ public class DynamoDBRepositoryFactory extends RepositoryFactorySupport {
 	 * @return
 	 */
 	@SuppressWarnings({ "unchecked", "rawtypes" })
-	protected <T, ID extends Serializable> DynamoDBCrudRepository<?, ?> getDynamoDBRepository(RepositoryMetadata metadata) {
-		return new SimpleDynamoDBPagingAndSortingRepository(getEntityInformation(metadata.getDomainType()), dynamoDBOperations,
-				getEnableScanPermissions(metadata));
+	protected <T, ID extends Serializable> DynamoDBCrudRepository<?, ?> getDynamoDBRepository(
+			RepositoryMetadata metadata) {
+		return new SimpleDynamoDBPagingAndSortingRepository(getEntityInformation(metadata.getDomainType()),
+				dynamoDBOperations, getEnableScanPermissions(metadata));
 	}
 
 	protected EnableScanPermissions getEnableScanPermissions(RepositoryMetadata metadata) {
 		return new EnableScanAnnotationPermissions(metadata.getRepositoryInterface());
-	}
-
-	@Override
-	protected Object getTargetRepository(RepositoryMetadata metadata) {
-
-		return getDynamoDBRepository(metadata);
-
 	}
 
 	@Override
@@ -90,6 +80,11 @@ public class DynamoDBRepositoryFactory extends RepositoryFactorySupport {
 
 	private static boolean isQueryDslRepository(Class<?> repositoryInterface) {
 		return QUERY_DSL_PRESENT && QueryDslPredicateExecutor.class.isAssignableFrom(repositoryInterface);
+	}
+
+	@Override
+	protected Object getTargetRepository(RepositoryInformation metadata) {
+		return getDynamoDBRepository(metadata);
 	}
 
 }
